92fe2813a4d84e3b8cb07f1f5bfb673e1dafa251
Adriano Finozzi Molero <adrianomolero@terra.com.br>
2020-05-18 16:36:15 -0300

    Esqueceu a senha

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -4,30 +4,36 @@ namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
 use \Hcode\Model;
+use \Hcode\Mailer;
 
 class User extends Model {
 
 	const SESSION = "User";
+	const SECRET = "HcodePhp7_Secret";
+	const SECRET_IV = "HcodePhp7_Secret_IV";
 
-	public static function login ($login, $password) 
+	public static function login($login, $password)
 	{
 
 		$sql = new Sql();
 
-		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN" , array(
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b ON a.idperson = b.idperson WHERE a.deslogin = :LOGIN", array(
 			":LOGIN"=>$login
-		));
+		)); 
 
 		if (count($results) === 0)
 		{
-			throw new \Exception("Usuário inexistente ou senha inválida");
+			throw new \Exception("Usuário inexistente ou senha inválida.");
 		}
 
 		$data = $results[0];
 
-		if (password_verify($password, $data["despassword"]) === true) 
+		if (password_verify($password, $data["despassword"]) === true)
 		{
+
 			$user = new User();
+
+			$data['desperson'] = utf8_encode($data['desperson']);
 
 			$user->setData($data);
 
@@ -36,14 +42,17 @@ class User extends Model {
 			return $user;
 
 		} else {
-			throw new \Exception("Usuário inexistente ou senha inválida");
+			throw new \Exception("Usuário inexistente ou senha inválida.");
 		}
+
 	}
 
 	public static function verifyLogin($inadmin = true)
 	{
 
 		//session_unset();
+
+		//cuidado quando o usuário não for admin
 
 		if (    !isset($_SESSION[User::SESSION])
 			 || !$_SESSION[User::SESSION]
@@ -58,7 +67,7 @@ class User extends Model {
 
 	public static function logout()
 	{
-		$_SESSION[User::SESSION] == NULL;
+		$_SESSION[User::SESSION] = NULL;
 	}
 
 	public static function listAll()
@@ -126,6 +135,137 @@ class User extends Model {
 		$sql = new Sql();
 
 		$sql->query("CALL sp_users_delete(:iduser)", array(
+			":iduser"=>$this->getiduser()
+		));
+
+	}
+
+public static function getForgot($email, $inadmin = true)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT *
+			FROM tb_persons a
+			INNER JOIN tb_users b USING(idperson)
+			WHERE a.desemail = :email;
+		", array(
+			":email"=>$email
+		));
+
+		if (count($results) === 0)
+		{
+
+			throw new \Exception("Não foi possível recuperar a senha.");
+
+		}
+		else
+		{
+
+			$data = $results[0];
+
+			$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+				":iduser"=>$data['iduser'],
+				":desip"=>$_SERVER['REMOTE_ADDR']
+			));
+
+			if (count($results2) === 0)
+			{
+
+				throw new \Exception("Não foi possível recuperar a senha.");
+
+			}
+			else
+			{
+
+				$dataRecovery = $results2[0];
+
+				$code = openssl_encrypt($dataRecovery['idrecovery'], 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+				$code = base64_encode($code);
+
+				if ($inadmin === true) {
+
+					$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+
+				} else {
+
+					$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+					
+				}				
+
+				$mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da Hcode Store", "forgot", array(
+					"name"=>$data['desperson'],
+					"link"=>$link
+				));				
+
+				$mailer->send();
+
+				return $link;
+
+			}
+
+		}
+
+	}
+
+	public static function validForgotDecrypt($code)
+	{
+
+		$code = base64_decode($code);
+
+		$idrecovery = openssl_decrypt($code, 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT *
+			FROM tb_userspasswordsrecoveries a
+			INNER JOIN tb_users b USING(iduser)
+			INNER JOIN tb_persons c USING(idperson)
+			WHERE
+				a.idrecovery = :idrecovery
+				AND
+				a.dtrecovery IS NULL
+				AND
+				DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW();
+		", array(
+			":idrecovery"=>$idrecovery
+		));
+
+		if (count($results) === 0)
+		{
+			throw new \Exception("Não foi possível recuperar a senha.");
+		}
+		else
+		{
+
+			return $results[0];
+
+		}
+
+	}
+
+	public static function setForgotUsed($idrecovery)
+	{
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_userspasswordsrecoveries SET dtrecovery = NOW() WHERE idrecovery = :idrecovery", array(
+
+			":idrecovery"=>$idrecovery
+		));
+	}
+
+	public function setPassword($password)
+	{
+
+		$sql = new Sql();
+
+		$sql->query("UPDATE tb_users SET despassword = :password WHERE iduser = :iduser", array(
+
+			":password"=>$password,
 			":iduser"=>$this->getiduser()
 		));
 
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -14,15 +14,15 @@ class Page {
 		"data"=>[]
 	];
 
-	public function __construct($opts = array(), $tpl_dir = "/views/") {
-
+	public function __construct($opts = array(), $tpl_dir = "/views/"){
+		
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
-						"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
-						"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
-						"debug"         => true // set to false to improve the speed
-					   );
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+			"debug"         => false
+	    );
 
 		Tpl::configure( $config );
 
@@ -34,7 +34,7 @@ class Page {
 
 	}
 
-	private function setData($data = array()) 
+	private function setData($data = array())
 	{
 
 		foreach ($data as $key => $value) {
@@ -43,19 +43,21 @@ class Page {
 
 	}
 
-	public function setTpl($name, $data = array(), $returnHTML = false) 
+	public function setTpl($name, $data = array(), $returnHTML = false)
 	{
 
 		$this->setData($data);
 
 		return $this->tpl->draw($name, $returnHTML);
+
 	}
 
-	public function __destruct() {
+	public function __destruct(){
 
 		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
 	}
+
 }
 
  ?>
