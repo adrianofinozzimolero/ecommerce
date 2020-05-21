739bbe9449ea967965d9d2c521b0e953c4c95e31
Adriano Finozzi Molero <adrianomolero@terra.com.br>
2020-05-21 10:59:12 -0300

    Login no carrinho

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -11,6 +11,7 @@ class User extends Model {
 	const SESSION = "User";
 	const SECRET = "HcodePhp7_Secret";
 	const SECRET_IV = "HcodePhp7_Secret_IV";
+	const ERROR = "UserError";
 
 	public static function getFromSession()
 	{
@@ -131,9 +132,9 @@ class User extends Model {
 		$sql = new Sql();
 
 		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
-			":desperson"=>$this->getdesperson(),
+			":desperson"=>utf8_decode($this->getdesperson()),
 			":deslogin"=>$this->getdeslogin(),
-			":despassword"=>$this->getdespassword(),
+			":despassword"=>User::getPasswordHash($this->getdespassword()),
 			":desemail"=>$this->getdesemail(),
 			":nrphone"=>$this->getnrphone(),
 			":inadmin"=>$this->getinadmin()
@@ -163,9 +164,9 @@ class User extends Model {
 
 		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
 					":iduser"=>$this->getiduser(),
-					":desperson"=>$this->getdesperson(),
+					":desperson"=>utf8_decode($this->getdesperson()),
 					":deslogin"=>$this->getdeslogin(),
-					":despassword"=>$this->getdespassword(),
+					":despassword"=>User::getPasswordHash($this->getdespassword()),
 					":desemail"=>$this->getdesemail(),
 					":nrphone"=>$this->getnrphone(),
 					":inadmin"=>$this->getinadmin()
@@ -316,6 +317,31 @@ public static function getForgot($email, $inadmin = true)
 		));
 
 	}
+
+	public static function setError($msg)
+	{
+
+		$_SESSION[User::ERROR] = $msg;
+
+	}
+
+	public static function getError()
+	{
+
+		$msg = (isset($_SESSION[User::ERROR]) && $_SESSION[User::ERROR]) ? $_SESSION[User::ERROR] : '';
+
+		User::clearError();
+
+		return $msg;
+
+	}
+
+	public static function clearError()
+	{
+
+		$_SESSION[User::ERROR] = NULL;
+
+	}	
 }
 
  ?>
